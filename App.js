9a64af84a1914dc85282de1962cf41e70026bf2e
Ben <bennnnnnnvu@gmail.com>
2025-05-02 18:22:21 -0700

    barebones intro to building a basic goal tracker app learning react native

---
+++ App.js
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput  style={styles.textInput} placeholder="Your course goal!"></TextInput>
+        <Button title="Add Goal"></Button>
+      </View>
+
+      <View>
+        <Text>List of goals...</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    padding: 50,
+  },
+
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#CCCCCC",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
+  }
+});
