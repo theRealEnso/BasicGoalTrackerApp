5d6432b4b6af6a84d233be837b33b1b6a506b533
Ben <bennnnnnnvu@gmail.com>
2025-05-05 01:31:12 -0700

    add additional styling to the goalItem component when user presses on the list item

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -8,24 +8,34 @@ export const GoalItem = ({goalItemData, setGoalsList}) => {
     };
 
     return (
-        <Pressable onPress={() => handleGoalRemoval(id)}>
-            <View>
+        <View style={styles.goalItem}>
+            <Pressable 
+                onPress={() => handleGoalRemoval(id)} 
+                android_ripple={{color: "#210644"}}
+                style={({pressed}) => pressed && styles.pressedItem} // can pass a function into the style prop. This function gets automatically called by Pressable whenever the `pressed` state changes => automatically get an object containing information about the `pressed` state, so we can destructure `pressed` off of this object directly. It is the same as doing (pressedData) => pressedData.pressed && styles.pressedItem
+            >
                 {/* property `item` on goalItemData contains the actual element in the goalsList array */}
                 {/* `text` property on goalItemData.item.text is pulling the text property off of each element stored in the array */}
-                <Text style={styles.goalItem}>{text}</Text> 
-            </View>
-        </Pressable>
-
+                <Text style={styles.goalText}>{text}</Text> 
+            </Pressable>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
     goalItem: {
         backgroundColor: "#7627f5",
-        color: "#ffffff",
         borderRadius: 8,
-        padding: 10,
         marginBottom: 10,
         width: "90%",
       },
+      
+      goalText: {
+        color: "#ffffff",
+        padding: 10,
+      },
+
+      pressedItem: {
+        opacity: 0.5,
+      }
 })
