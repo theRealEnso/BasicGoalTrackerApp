bb0ab988650d73093b86da5639e025bfd525d949
Ben <bennnnnnnvu@gmail.com>
2025-05-07 15:31:20 -0700

    final styling finishing touches

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
 
 //import components
 import { GoalItem } from './components/GoalItem';
@@ -10,44 +10,60 @@ export default function App() {
 
   const [textError, setTextError] = useState("");
   const [goalsList, setGoalsList] = useState([]); // to be a list of objects
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(true);
+  }
 
   return (
-    // main view
-    <View style={styles.appContainer}>
-      {/* view / div that holds the input and button */}
-      <View style={styles.inputView}>
-        <GoalInput setTextError={setTextError} setGoalsList={setGoalsList}></GoalInput>
+    <>
+      <StatusBar style="light"></StatusBar>
+      {/* main view */}
+      <View style={styles.appContainer}>
+        <View style={styles.buttonContainer}>
+          <Button title="Add New Goal!" color="#7627f5" onPress={toggleModal}></Button>
+        </View>
+        
+        {/* view / div that holds the input and button */}
+        <View style={styles.inputView}>
+          {/* conditionally render GoalInput component */}
+          {
+            showModal && <GoalInput setTextError={setTextError} setGoalsList={setGoalsList} showModal={showModal} setShowModal={setShowModal}></GoalInput>
+          }
+          
+          {/* conditionally render textError */}
+          {
+            textError && (
+              <View>
+                <Text style={styles.textError}>{textError}</Text>
+              </View>
+            )
+          }
 
-        {
-          textError && (
-            <View>
-              <Text style={styles.textError}>{textError}</Text>
-            </View>
-          )
-        }
+        </View>
 
+        {/* view / div that holds the list of goals */}
+        <View style={styles.goalsContainer}>
+        {/* using FlatList instead of ScrollView for optimization => FlatList only renders what is necessary on the phone's current view and lazy loads the remaining data as the user continues to scroll. This is better than ScrollView because ScrollView will render everything, regardless if we have a list that is 50, 100, or 1000 items long. */}
+        {/*FlatList needs data and renderItem props */}
+          <FlatList 
+            data={goalsList} 
+            renderItem={(itemData) => { //callback function passed to renderItem automatically receives an object as an input-- each object contains metadata & represents each element in goalsList array
+            return (
+              <GoalItem goalItemData={itemData} setGoalsList={setGoalsList}></GoalItem> //property `item` on object contains our actual array element
+            )
+          }}
+          //FlatList requires a key, similar to when we map through array elements
+          //since our data doesn't explicitly have a `key` property but instead has an `id` property, we can use `keyExtractor` prop to extract the `id` property on each array element and map it to a FlatList's internal key.
+          keyExtractor={(item, index) => { // callback function automatically receives each array element and its index
+            return item.id;
+          }}
+          >
+          </FlatList>
+        </View>
       </View>
-
-      {/* view / div that holds the list of goals */}
-      <View style={styles.goalsContainer}>
-      {/* using FlatList instead of ScrollView for optimization => FlatList only renders what is necessary on the phone's current view and lazy loads the remaining data as the user continues to scroll. This is better than ScrollView because ScrollView will render everything, regardless if we have a list that is 50, 100, or 1000 items long. */}
-      {/*FlatList needs data and renderItem props */}
-        <FlatList 
-          data={goalsList} 
-          renderItem={(itemData) => { //callback function passed to renderItem automatically receives an object as an input-- each object contains metadata & represents each element in goalsList array
-          return (
-            <GoalItem goalItemData={itemData} setGoalsList={setGoalsList}></GoalItem> //property `item` on object contains our actual array element
-          )
-        }}
-        //FlatList requires a key, similar to when we map through array elements
-        //since our data doesn't explicitly have a `key` property but instead has an `id` property, we can use `keyExtractor` prop to extract the `id` property on each array element and map it to a FlatList's internal key.
-        keyExtractor={(item, index) => { // callback function automatically receives each array element and its index
-          return item.id;
-        }}
-        >
-        </FlatList>
-      </View>
-    </View>
+    </>
   );
 }
 
@@ -57,6 +73,12 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingHorizontal: 16,
     backgroundColor: "#0a001a",
+  },
+
+  buttonContainer: {
+    padding: 8,
+    position: "relative",
+    top: 30,
   },
 
   inputView: {
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
-import { StyleSheet, View, TextInput, Button } from "react-native"
+import { StyleSheet, View, TextInput, Button, Modal, Image } from "react-native"
 
-export const GoalInput = ({setTextError, setGoalsList}) => {
+export const GoalInput = ({setTextError, setGoalsList, showModal, setShowModal}) => {
     const [textInput, setTextInput] = useState("");
 
     const handleInputChange = (enteredText) => {
@@ -10,13 +10,14 @@ export const GoalInput = ({setTextError, setGoalsList}) => {
         setTextInput(enteredText);
     };
     
-    const handlePress = () => {
+    const handleAddGoalPress = () => {
         if(textInput.length > 0){
           // setGoalsList([...goalsList, textInput]); // this works...
           // but, recommended way of updating state if state updates depend on the previous state -- pass a callback function that automatically receives the existing state as an input
           setGoalsList((currentGoals) => [...currentGoals, {text: textInput, id: Math.random().toString()}]);
           setTextInput("");
           setTextError("");
+          setShowModal(false);
     
         } else {
           setTextError("Cannot add an empty goal!");
@@ -27,25 +28,61 @@ export const GoalInput = ({setTextError, setGoalsList}) => {
     };
 
     return (
-        <View style={styles.inputContainer}>
-            <TextInput value={textInput} style={styles.textInput} placeholder="Your course goal!" onChangeText={handleInputChange}></TextInput>
-            <Button title="Add Goal" onPress={handlePress}></Button>
-        </View>
+        // modal has built in `visible` and `animationType` props we can use to add a slide effect when modal is toggled
+        <Modal visible={showModal} animationType="slide"> 
+            <View style={styles.inputContainer}>
+                <Image source={require('../assets/images/goal.png')} style={styles.image}></Image>
+                <TextInput value={textInput} style={styles.textInput} placeholder="Your course goal!" onChangeText={handleInputChange}></TextInput>
+                
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button title="Add Goal" color={"#b180f0"} onPress={handleAddGoalPress}></Button>
+                    </View>
+                    <View style={styles.button}>
+                        <Button title="Cancel" color={"#f31282"} onPress={() => setShowModal(false)}></Button>
+                    </View>
+                    
+                </View>
+                
+            </View>
+        </Modal>
     )
-}
+};
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flexDirection: "row",
-        justifyContent: "space-between",
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 16,
+        backgroundColor: "#0a001a",
     },
 
+    buttonContainer: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+    },
+
+    button: {
+        width: "30%",
+        marginHorizontal: 8,
+    }
+,
     textInput: {
         borderWidth: 1,
-        borderColor: "#CCCCCC",
-        width: "70%",
-        marginRight: 8,
-        padding: 8,
-        backgroundColor: "#ffffff",
+        borderColor: "#330485",
+        borderRadius: 6,
+        width: "100%",
+        marginBottom: 10,
+        padding: 16,
+        backgroundColor: "#330485",
+        color: "#ffffff"
     },
+
+    image: {
+        width: 100,
+        height: 100,
+        margin: 20,
+    }
 })
